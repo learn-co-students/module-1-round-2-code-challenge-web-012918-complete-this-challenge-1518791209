1bbf384940e782f3ede8f4576fa0b8ab030c1dd2
yw544 <30517389+yw544@users.noreply.github.com>
2018-02-16 10:45:00 -0500

    Revert "Done."

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -9,20 +9,7 @@ class Viewer
   end
 
   def self.all
-    #returns all the viewers
     @@all
   end
-
-  def add_to_watchlist(movies)
-    movies.each do |movie|
-      Watchlist.new(movie, self)
-    end
-#receive array of one or more movie instances and add to viewers watchlist
-  end
-
-  def rate_movie(movie, rating)
-    Watchlisting.new(self, movie, rating)
-    #the rating and movie should be assigned to the current individual viewers instance.
-  end
-
+  
 end
--- app/models/watchlisting.rb
+++ app/models/watchlisting.rb
@@ -1,19 +1,4 @@
 class WatchListing
-attr_accessor :viewer, :movie, :rating
-
-@@all = []
-
-def initialize(viewer, movie, rating)
-  @viewer = viewer
-  @movie = movie
-  @rating = rating
-  @@all << self
-end
-
-def self.all
-  @@all
-end
-
 
 
 end
