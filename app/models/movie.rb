682617b2bfcdc956748d6969aca6ca687951b196
github username <github email address>
2018-02-16 10:38:19 -0500

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,7 +9,50 @@ class Movie
   end
 
   def self.all
+    #returns an array of all movies
     @@all
+  end
+
+  def watchlistings
+    WatchListing.all.select do |list|
+      list.movie == self
+    end
+#return array of ALL watchlist objects for the movie
+  end
+
+  def viewers
+
+    watchlistings.map do |movie|
+    movie.viewer
+    end
+
+    #return all viewers who added this movie
+  end
+
+  def average_rating
+    sum = 0
+    watchlistings.each do |list|
+      sum += list.rating
+    end
+    sum/viewers.length
+    #returns average of all ratings across viewers
+  end
+
+  def highest_rated
+    all_movie = WatchListing.all.map do |list|
+     list.movie
+     end
+     uniq_movie = all_movie.uniq
+     uniq_movie.each do |movie|
+       movie.average_rating 
+
+     binding.pry
+     # each_movie.all.map.do |movie|
+     # movie.average_rating
+   # end
+
+     #sort array desc, take first oen
+    #return movie with highest average across viewers watchlistings
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,15 @@ def reload
   load 'config/environment.rb'
 end
 
+york = Viewer.new("york")
+wang = Viewer.new("wang")
+terrance = Viewer.new("terrance")
+niky = Viewer.new("niky")
+red_bull = Movie.new("red bull")
+flatiron = Movie.new("flatiron")
+WatchListing.new(york, red_bull, 8)
+WatchListing.new(wang, flatiron, 7)
+WatchListing.new(terrance, red_bull, 9)
+WatchListing.new(niky, red_bull, 10)
+
 Pry.start
